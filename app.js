a8bf77d1797157a4cad2d9f77bef32de38961aad
Mario Zamora <maza1387@gmail.com>
2017-07-26 10:01:02 -0700

    Add error handling to routes

--- app.js
+++ app.js
@@ -12,6 +12,24 @@ app.use(jsonParser());
 
 app.use('/questions', routes);
 
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err)
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  // err.status would be undefined if this handler was receiving an internal server error
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+})
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
