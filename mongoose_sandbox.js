2286f6c0ece0116ff59fbe833d43d68eb8681a39
Mario Zamora <maza1387@gmail.com>
2017-07-26 18:50:28 -0700

    Work with .create(), Pre Save Hooks, Static and Instance Methods

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -16,32 +16,90 @@ db.once('open', function() {
 
   const Schema = mongoose.Schema;
   const AnimalSchema = new Schema({
-    type: String,
-    color: String,
-    size: String,
-    mass: Number,
-    name: String
+    type:  {type: String, default: 'goldfish'},
+    color: {type: String, default: 'golden'},
+    size:  String,
+    mass:  {type: Number, default: 0.007},
+    name:  {type: String, default: 'Angela'}
   })
+
+  AnimalSchema.pre('save', function(next) {
+    if (this.mass >= 100) {
+      this.size = 'big';
+    } else if (this.mass >= 5 && this.mass < 100) {
+      this.size = 'medium';
+    } else {
+      this.size = 'small';
+    }
+    next();
+  })
+
+  AnimalSchema.statics.findSize = function(size, callback) {
+    return this.find({size: size}, callback)
+  }
+
+  AnimalSchema.methods.findSameColor = function(callback) {
+    return this.model('Animal').find({color: this.color}, callback);
+  }
 
   const Animal = mongoose.model('Animal', AnimalSchema);
 
   var elephant = new Animal({
     type: 'elephant',
     color: 'gray',
-    size: 'big',
     mass: 6000,
     name: 'Lawrence'
+  });
+
+  var animal = new Animal({});
+
+  var whale = new Animal({
+    type: 'whale',
+    mass: 190500,
+    name: 'Fig'
   })
 
-  elephant.save(function(err) {
-    if (err) {
-      console.error('Save Failed', err);
-    } else {
-      console.log('Saved!');
-    }
+  var animalData = [
+    {
+      type: 'mouse',
+      color: 'gray',
+      mass: 0.035,
+      name: 'Marvin'
+    },
+    {
+      type: 'nutria',
+      color: 'brown',
+      mass: 6.35,
+      name: 'Gretchen'
+    },
+    {
+      type: 'wolf',
+      color: 'gray',
+      mass: 45,
+      name: 'Iris'
+    },
+    elephant,
+    animal,
+    whale
+  ]
 
-    db.close(function() {
-      console.log('db connection closed');
-    });
+  Animal.remove({}, function(err) {
+    if (err) console.error(err);
+    Animal.create(animalData ,function(err, animals) {
+      if (err) console.error(err);
+      Animal.findOne({type: 'elephant'}, function(err, elephant) {
+        elephant.findSameColor(function(err, animals) {
+          if (err) console.error(err);
+          animals.forEach(function(animal) {
+            console.log(animal.name + ' the ' + animal.color + ' ' + animal.type + ' is a ' + animal.size + '-sized animal.');
+          })
+          db.close(function() {
+            console.log('db connection closed');
+          })
+        })
+
+      })
+    })
   })
+
 });
